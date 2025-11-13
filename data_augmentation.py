7f1ffa5b1aecc7abeabb8fffb6bd1b25c5fabbcc
KarenLi <chinalilikun@126.com>
2025-11-13 10:22:56 +0800

    add readme

--- data_augmentation.py
+++ data_augmentation.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import torchvision
 from torchvision import transforms
 
-# 基于现有模型vgg进行迁移学习
+# 数据增强， 放到transforms.Compose 中,解决过拟合、数据量不足，提高模型返回能力
 # 基于 cnn_weather_recognition.py 中的数据路径
 # 从https://www.kaggle.com/datasets/saurabhshahane/multi-class-weather-dataset 下载数据，放到项目的./dataset 中
 
@@ -18,37 +18,63 @@ base_dir = './dataset'
 train_dir = os.path.join(base_dir, 'train')
 test_dir = os.path.join(base_dir, 'test')
 
-transfrom = transforms.Compose([
-    transforms.Resize((96, 96)),  # 专门进行图片缩放的方法，都转换为96*96
+# 数据增强的一些方式
+# transforms.Random* 随机的一些转换，裁剪，锐化，反转，旋转
+# transforms.CenterCrop 从中间剪裁
+# transforms.RandomRotation 随机旋转
+# transforms.RandomHorizontalFlip 随机水平反转
+# transforms.RandomVerticalFlip 随机垂直反转
+# transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1) 对某个数据进行抖动 brightness 亮度，contrast 对比度，saturation饱和度，hue 颜色
+# transforms.RandomGrayscale 随机灰度化
+# transforms.RandomCrop 随机裁剪
+
+# 数据增强只会加在训练数据上
+train_transfrom = transforms.Compose([
+    transforms.Resize((224, 224)),  # 专门进行图片缩放的方法，都转换为224*224
+    transforms.RandomCrop(192), #随机剪切图片
+    transforms.RandomHorizontalFlip(), # 水平反转 默认50%的可能性被反转
+    transforms.RandomVerticalFlip(), # 垂直反转
+    transforms.RandomRotation(0.4),# 随机选择的角度，给一个数字，就按照这个数字的-degree，degree去选择，给两个数字，就在这个范围内旋转
     transforms.ToTensor(),  # 转换为张量
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                          std=[0.229, 0.224, 0.225])
     # 正则化，mean 均值，std标准差，这些数值是在ImageNet数据集上计算得到的统计量,ImageNet: 包含1400万张图像，22000个类别,在数百万张图像上计算每个通道的均值和标准差
 ])
-train_ds = torchvision.datasets.ImageFolder(root=train_dir, transform=transfrom)  # 训练数据集，要求同类的数据在同一个文件夹内
-test_ds = torchvision.datasets.ImageFolder(root=test_dir, transform=transfrom)
+# 测试数据不需要数据增强
+test_transfrom = transforms.Compose([
+    transforms.Resize((224, 224)),  # 专门进行图片缩放的方法，都转换为224*224
+    transforms.ToTensor(),  # 转换为张量
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+    # 正则化，mean 均值，std标准差，这些数值是在ImageNet数据集上计算得到的统计量,ImageNet: 包含1400万张图像，22000个类别,在数百万张图像上计算每个通道的均值和标准差
+])
+train_ds = torchvision.datasets.ImageFolder(root=train_dir, transform=train_transfrom)  # 训练数据集，要求同类的数据在同一个文件夹内
+test_ds = torchvision.datasets.ImageFolder(root=test_dir, transform=test_transfrom)
 batch_size = 32
 train_dl = torch.utils.data.DataLoader(train_ds, batch_size=batch_size, shuffle=True,
                                        drop_last=True)  # 最后一批数据如果不满足数量就丢掉
 test_dl = torch.utils.data.DataLoader(test_ds, batch_size=batch_size * 4)  # 测试数据不需要随机
 
-model = torchvision.models.vgg16(pretrained=True)  # BN 是2015年出来的，会有带bn和不带bn版本的模型，pretrained 为True加载在 ImageNet 数据集上预训练好的权重,第一次加载会下载 模型
+model = torchvision.models.vgg16(
+    pretrained=True)  # BN 是2015年出来的，会有带bn和不带bn版本的模型，pretrained 为True加载在 ImageNet 数据集上预训练好的权重,第一次加载会下载 模型
 for param in model.features.parameters():
-    param.requires_grad = False # 原有模型中的参数，设置为不可训练，保持和之前一样
+    param.requires_grad = False  # 原有模型中的参数，设置为不可训练，保持和之前一样
 
 # 修改原网络输出层结构
-#model.classifier[-1].out_features = 4 #原模型最后的输出类别为1000，即可以识别1000种类，我们要修改为4，输出类别为4种
+# model.classifier[-1].out_features = 4 #原模型最后的输出类别为1000，即可以识别1000种类，我们要修改为4，输出类别为4种
 # 另一种修改输出层的写法，把输出层直接改掉
-model.classifier[-1]= torch.nn.Linear(in_features=model.classifier[-1].in_features, out_features=4)
+model.classifier[-1] = torch.nn.Linear(in_features=model.classifier[-1].in_features, out_features=4)
 
 # 拷贝到 GPU上
 device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')  # 如果GPU可用，就用GPU，cuda 0 第一个GPU，独立显卡，否则用CPU
 
 model = model.to(device)
 
-
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 loss_fn = nn.CrossEntropyLoss()
+
+# 学习率衰减 pytorch 里封装好的 stepLR 根据训练次数来衰减，学习率越到后面就越小，step_size多少步衰减一次，gamma 默认是0.1 衰减率
+step_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
 
 def fit(epochs, model, train_dl, test_dl):
@@ -69,10 +95,11 @@ def fit(epochs, model, train_dl, test_dl):
             correct += (y_pred == y).sum().item()
             total += y.size(0)  # 样本个数
             running_loss += loss.item()
+    step_lr_scheduler.step()
     epoch_loss = running_loss / len(train_dl.dataset)
     epoch_acc = correct / total
 
-    # 测试过程
+    # 测试过程,测试过程是推测，不需要学习率衰减
     test_correct = 0
     test_total = 0
     test_running_loss = 0
@@ -97,7 +124,7 @@ def fit(epochs, model, train_dl, test_dl):
     return epoch_loss, epoch_acc, test_epoch_loss, test_epoch_acc
 
 
-epochs = 10 # 训练10次就可以了，vgg比较慢
+epochs = 10  # 训练10次就可以了，vgg比较慢
 train_loss = []
 train_acc = []
 test_loss = []
@@ -111,12 +138,12 @@ for epoch in range(epochs):
     test_acc.append(test_epoch_acc)
 # 第一次训练表现就很好
 
-plt.plot(range(1,epochs+1),train_loss, label='train loss')
-plt.plot(range(1,epochs+1),test_loss, label='test loss')
+plt.plot(range(1, epochs + 1), train_loss, label='train loss')
+plt.plot(range(1, epochs + 1), test_loss, label='test loss')
 plt.legend()
 plt.show()
 
-plt.plot(range(1,epochs+1),train_acc, label='train accuracy')
-plt.plot(range(1,epochs+1),test_acc, label='test accuracy')
+plt.plot(range(1, epochs + 1), train_acc, label='train accuracy')
+plt.plot(range(1, epochs + 1), test_acc, label='test accuracy')
 plt.legend()
 plt.show()
--- readme.md
+++ readme.md
@@ -25,3 +25,13 @@ torch.cuda.get_device_name(0)
 ## cnn_weather_recognition 中的图片数据来源 kaggle
 https://www.kaggle.com/datasets/saurabhshahane/multi-class-weather-dataset
 
+### 构建卷积神经网络，对MNIST中的灰度数字进行识别，将模型放到GPU上进行训练
+cnn_digital_recognition.py
+### 对kaggle中四种天气进行卷积神经网络分类，增加dropout（随机关闭一些神经节）和BN层（批量归一化）
+cnn_weather_recognition.py
+### 基于现有模型vgg进行迁移学习
+transfer_learning_base_vgg.py
+### 学习率衰减，越接近参数值时，学习率应该变得小一点，越接近于极值
+decay_rate.py
+### 数据增强， 放到transforms.Compose 中,解决过拟合、数据量不足，提高模型返回能力
+data_augmentation.py 
